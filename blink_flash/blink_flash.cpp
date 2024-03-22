d05ab6a2420ce27f869dc2171ba318f780a50b04
Kadoyu <kadoyu10@gmail.com>
2024-03-23 07:47:13 +0900

    Add blink_flash

---
+++ blink_flash/blink_flash.cpp
@@ -0,0 +1,62 @@
+/**
+ * 30回Lチカしたらメモリリセットします。
+ * 不揮発メモリに設定値を保存するため切断してもその前の数字から始まります。
+ * メモリは最終セクタの4kByteを使用します。
+ */
+#include <iostream>
+#include "pico/stdlib.h"
+#include "pico/cyw43_arch.h"
+#include "FlashPROM.h"
+
+int size = 512;
+int addr = 0;
+uint count;
+
+int main()
+{
+    stdio_init_all();
+    if (cyw43_arch_init())
+    {
+        printf("Wi-Fi init failed");
+        return -1;
+    }
+
+    EEPROM.begin(size);
+    count = EEPROM.read(0);
+    std::cout << "Read Data " << count << std::endl;
+
+    while (true)
+    {
+        EEPROM.write(addr, count);
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
+        sleep_ms(250);
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
+        sleep_ms(250);
+        std::cout << "Blink Count: " << count << std::endl;
+
+        if (EEPROM.commit())
+        {
+            printf("EEPROM successfully comitted\n");
+        }
+        else
+        {
+            printf("ERROR! EEPROM commit failed\n");
+        }
+
+        if (count >= 30)
+        {
+            if (EEPROM.reset())
+            {
+                printf("EEPROM successfully reset\n");
+            }
+            else
+            {
+                printf("ERROR! EEPROM reset failed\n");
+            }
+        }
+        count = EEPROM.read(0);
+        count++;
+    }
+
+    return 0;
+}
---
+++ blink_usb/blink_usb.cpp
@@ -0,0 +1,37 @@
+/**
+ * シリアルリードした数だけLチカスルコード
+ * ちなみにPicoのSerialSpeedはデフォルトで115200bps
+ */
+#include <iostream>
+#include "pico/stdlib.h"
+#include "pico/cyw43_arch.h"
+int main()
+{
+    int interval;
+
+    stdio_init_all();
+
+    if (cyw43_arch_init())
+    {
+        printf("Wi-Fi init failed");
+        return -1;
+    }
+
+    while (true)
+    {
+        //scanf("%d", &interval);
+        std::cout << "Enter a number: ";
+        std::cin >> interval;
+
+        while (interval != 0)
+        {
+            //printf("Burn: %d\n", interval);
+            std::cout << "Burn: " << interval << std::endl;
+            cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
+            sleep_ms(500);
+            cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
+            sleep_ms(500);
+            interval--;
+        }
+    }
+}
